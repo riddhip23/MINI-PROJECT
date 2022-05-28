64bc5977271ac4a378f9458b24637225ab335c66
unknown <riddhi.patil1@walchandsangli.ac.in>
2022-05-28 10:42:36 +0530

    First commit

---
+++ digimess/android/app/src/main/kotlin/com/example/digimess/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.digimess
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ groceries_calculation/android/app/src/main/kotlin/com/example/groceries_calculation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.groceries_calculation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
